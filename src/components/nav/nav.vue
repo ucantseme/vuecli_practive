6b901db58c26a838b9f2040bf04c209560ee0c22
siang <weishang@4hotel.tw>
2018-06-08 16:53:26 +0800

    nav style

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="nav">
-        <router-link to="/goods" class="nav-item goods">點餐</router-link>
-        <router-link to="/ratings" class="nav-item ratings">評價</router-link>
-        <router-link to="/seller" class="nav-item seller">商家</router-link>
+        <router-link to="/goods" class="nav-item goods">
+            點餐
+            <i class="line"></i>
+        </router-link>
+        <router-link to="/ratings" class="nav-item ratings">
+            評價
+            <i class="line"></i>
+        </router-link>
+        <router-link to="/seller" class="nav-item seller">
+            商家
+            <i class="line"></i>
+        </router-link>
     </div>
 </template>
 
@@ -21,9 +30,21 @@
     font-size: 13px;
     text-decoration: none;
     color: #666666;
+    position: relative;
+    &.active{
+        color: #FFBB22; 
+    }
+    &.active .line{
+        width: 20px;
+        height: 2px;
+        display: inline-block;
+        background-color: #FFBB22;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%)
+    }
   }
-  &.active{
-     color: #FFBB22; 
-  }
+
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -24,5 +24,6 @@ export default new Router({
       path: '/seller',
       component: goods
     },
-  ]
+  ],
+  linkActiveClass: 'active'
 })
