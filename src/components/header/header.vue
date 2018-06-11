739727bb5ba3653e70e7ec25906c96d9f5d282c2
siang <weishang@4hotel.tw>
2018-06-11 13:36:48 +0800

    header complete

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -23,18 +23,65 @@
 				{{poiInfo.name}}
 			</div>
 			<div class="collect">
-				<img src="../../../resource/img/star.png">
+				<img src="./star.png">
 				<span>收藏</span>
 			</div>
 		</div>
-		<div class="bulletin">
-
+		<div class="bulletin" v-if="poiInfo.discounts2">
+			<img :src="poiInfo.discounts2[0].icon_url">
+			<span class="text">{{poiInfo.discounts2[0].info}}</span>
+			<div class="detail" @click="bullentinOpen()">
+				{{poiInfo.discounts2.length}}個活動
+				<span class="icon-keyboard_arrow_right"></span>	
+			</div>
 		</div>
+		<transition name="detail">
+			<div class="bullentin-detail" v-show="isShow">
+				<div class="info">
+					<div class="main" :style="infoBg">
+						<div class="icon" :style="iconImg"></div>
+						<h3 class="name">{{poiInfo.name}}</h3>
+						<div class="score">
+							<star :score="poiInfo.wm_poi_score"></star>
+							<span>{{poiInfo.wm_poi_score}}</span>
+						</div>
+						<p class="tip">
+							{{poiInfo.min_price_tip}} <i>|</i>
+							{{poiInfo.shipping_fee_tip}} <i>|</i>
+							{{poiInfo.delivery_time_tip}}						
+						</p>
+						<p class="time">
+							配送時間:
+							{{poiInfo.shipping_time}}
+						</p>
+						<div class="discounts" v-if="poiInfo.discounts2">
+							<p>
+								<img :src="poiInfo.discounts2[0].icon_url">
+								<span>{{poiInfo.discounts2[0].info}}</span>
+							</p>
+						</div>
+					</div>
+					<div class="close" @click="bullentinClose()">
+						<span class="icon-close"></span>
+					</div>
+				</div>
+			</div>
+		</transition>
 	</div>
 </template>
 
 <script>
+
+import star from "@/components/star/star"
 export default{
+	data () {
+		return{
+			isShow: false,
+		}
+	},
+	components: {
+		star
+	},
 	props: {
 		poiInfo: {
 			type: Object,
@@ -47,7 +94,19 @@ export default{
 		},
 		iconImg () {
 			return "background-image: url("+this.poiInfo.pic_url+")"
+		},
+		infoBg () {
+			return "background-image: url("+this.poiInfo.poi_back_pic_url+")"
 		}
+	},
+	methods:{
+		bullentinOpen () {	
+			this.isShow = true
+		},
+		bullentinClose () {
+			this.isShow = false
+		},
+
 	}
 }
 </script>
@@ -58,11 +117,13 @@ export default{
 	width: $w;
 	height: $h;
 }
+$color_white: white;
 .header{
-	height: 160px;
+	@include size(100%,160px);
 	padding-top: 20px;
 	background-position: center 1px;
 	background-size: 110% 126%;
+	box-sizing: border-box;
 }
 .top{
 	@include size(calc(100% - 36px),31px);
@@ -74,7 +135,7 @@ export default{
 		@include size(50px,100%);
 		text-align: center;
 		line-height: 31px;
-		color: white;
+		color: $color_white;
 	}
 	.top-search{
 		@include size(100%);
@@ -83,7 +144,7 @@ export default{
 		.search-icon{
 			position: absolute;
 			@include size(28px,31px);
-			background: url('../../../resource/img/titans_h5_search@2x.png') no-repeat 11px center;
+			background: url('titans_h5_search@2x.png') no-repeat 11px center;
 			background-size: 13px 13px;
 		}
 		.search-bar{
@@ -103,9 +164,9 @@ export default{
 		display: flex;
 		.spelling-btn{
 			@include size(30px,17px);
-			color: white;
+			color: $color_white;
 			line-height: 17px;
-			border: 1px solid white;
+			border: 1px solid $color_white;
 			text-align: center;
 			float: left;
 			text-decoration: none;
@@ -118,7 +179,7 @@ export default{
 			.s-radius{
 				@include size(3px);
 				border-radius: 50%;
-				border: 1px solid white;
+				border: 1px solid $color_white;
 				display: block;
 				float: left;
 				margin-right: 1px;
@@ -142,7 +203,7 @@ export default{
 		padding: 18px 0 0 12px;
 		font-size: 16px;
 		font-weight: bold;
-		color: white;
+		color: $color_white;
 	}
 	.collect{
 		@include size(25px,37px);
@@ -154,11 +215,132 @@ export default{
 			padding-top: 6px;
 		}
 		span{
-			color: white;
+			color: $color_white;
 			font-size: 11px;
 			margin-top: 7px;
 		}
 	}
 }
-	
+.bulletin{
+	margin: 0 10px;
+	height: 16px;
+	font-size: 11px;
+	color: $color_white;
+	white-space: nowrap;
+	img{
+		@include size(16px);
+		margin-right: 6px;
+		float: left;
+	}
+	.text{
+		float: left;
+		line-height: 16px;
+	}
+	.detail{
+		float: right;
+		cursor: pointer;
+		span{
+			font-size: 11px;
+			line-height: 16px;
+		}
+	}
+}
+.bullentin-detail{
+	@include size(100%);
+	position: absolute;
+	left: 0;
+	top: 0;
+	z-index: 999;
+	background-color: rgba(98, 98, 98, 0.8);
+}
+.info{
+	@include size(100%);
+	padding: 43px 20px 125px;
+	box-sizing: border-box;
+}
+.main{
+	@include size(100%);
+	background-size: 100% 100%;
+	border-radius: 10px;
+	text-align: center;
+	.icon{
+		@include size(60px);
+		background-size: 135% 100%;
+		background-position: center;
+		border-radius: 5px;
+		display: inline-block;
+		margin-top: 40px;
+	}
+	.name{
+		font-size: 15px;
+		color: $color_white;
+		margin-top: 13px;
+		margin-bottom: 6px;
+	}
+	.score{
+		height: 10px;
+		line-height: 0;
+		text-align: center;
+		.star{
+			display: inline-block;
+			font-size: 0;
+		}
+		span{
+			color: $color_white;
+			margin-left: 4px;
+		}
+	}
+	.tip{
+		font-size: 11px;
+		color: #bababc;
+		margin-top: 8px;
+		i{
+			margin: 0 7px;
+		}
+	}
+	.time{
+		font-size: 11px;
+		color: #bababc;
+		margin-top: 13px;
+	}
+	.discounts{
+		margin-top: 21px;
+		padding: 0 20px;
+		p{
+			padding-top: 20px;
+			border-top: 1px solid #bababc;			
+			img{
+				@include size(16px);
+				vertical-align: middle;
+				margin-right: 10px;
+			}
+			span{
+				font-size:11px;
+				line-height: 16px;
+				color: $color_white;
+			}
+		}
+	}
+}
+.close{
+	@include size(40px);
+	margin: 20px auto 65px;	
+	border-radius: 50%;
+	border: 1px solid rgba(140,140,140,0.9);
+	background-color: rgba(118,118,118,0.7);
+	text-align: center;
+	cursor: pointer;
+	span{		
+		font-size: 14px;
+		line-height: 40px;
+		color: $color_white;
+	}
+}
+
+.detail-enter-active, .detail-leave-active{
+  transition: opacity 1s;
+}
+.detail-enter, .detail-leave-to{
+  opacity: 0;
+}	
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="star">
+        <span v-for="star in stars" :class="star" class="star-item"></span>
+    </div>
+</template>
+<script>
+    const Length = 5
+    const star_on = 'on'
+    const star_half = 'half'
+    const star_off = 'off'
+
+    export default{
+        props: {
+            score: {
+                type: Number,
+            }
+        },
+        computed: {
+            stars () {
+                let result = []
+                // 向下取0.5倍數
+                let score = Math.floor(this.score*2) / 2
+                // 小數, 半星
+                let decimal = score % 1 !== 0
+                // 整數, 全星
+                let integer = Math.floor(score)
+
+                for(let i=0;i<integer;i++){
+                    result.push(star_on)
+                }
+
+                if(decimal){
+                    result.push(star_half)
+                }
+                while (result.length < Length) {
+                    result.push(star_off)
+                }
+                return result
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+.star-item{
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    margin-right: 3px;
+    background-size: 10px 10px;
+    background-repeat: no-repeat;
+    &.on{
+        background-image: url('star24_on@2x.png');
+    }
+    &.half{
+        background-image: url('star24_half@2x.png');
+    }
+    &.off{
+        background-image: url('star24_off@2x.png');
+    }
+}
+
+</style>
